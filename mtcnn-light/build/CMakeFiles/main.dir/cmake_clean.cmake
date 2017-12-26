a23a12a947818cc9acd7345d10218052823e2f86
mechine-T <1033695480@qq.com>
2017-12-26 10:50:04 +0800

    my first project

---
+++ mtcnn-light/build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/pBox.cpp.o"
+  "CMakeFiles/main.dir/src/mtcnn.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/network.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
